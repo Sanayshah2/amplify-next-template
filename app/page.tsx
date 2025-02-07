4aa0b6022d2e01a96805051c5f960684036a3857
Sanay Shah <sanayshah2@gmail.com>
2025-02-06 16:48:14 -0800

    Add console.log()

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
+  console.log(process.env);
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
